50232394bbaba24462072b8e9c7849bb5543ee5e
Piotr Madeja <piotr.madeja19@gmail.com>
2018-04-10 12:26:25 +0200

    12.5

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+var url = 'http://api.icndb.com/jokes/random';  //adres do dowcipu
+
+var button = document.getElementById('get-joke'); //pobiera element po ID
+button.addEventListener('click', function(){  //dodaje listener do buttona
+  getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+window.onload= getJoke;
+
+function getJoke() {  //implementacja funkji
+  var xhr = new XMLHttpRequest(); //nowa instancja obiektu
+  xhr.open('GET', url);  //otwarcie polaczenia z wybranym adresem
+  xhr.addEventListener('load', function(){ //listener
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
